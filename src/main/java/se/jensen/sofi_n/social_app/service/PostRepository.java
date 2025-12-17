27d77a987af4b0fe46e049f49b21411b3af07f4e
Sofi Eklöf <sofieklofnaderi@icloud.com>
2025-12-17 11:40:04 +0100

    postcontroller går mot databas, delete metod i postservice

--- src/main/java/se/jensen/sofi_n/social_app/service/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package se.jensen.sofi_n.social_app.service;
-
-import org.springframework.data.repository.Repository;
-import se.jensen.sofi_n.social_app.model.Post;
-
-interface PostRepository extends Repository<Post, Long> {
-}
